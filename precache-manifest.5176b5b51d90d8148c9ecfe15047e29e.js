c78b6b19822101804a04363d66ba073502e7a077
Joseph Weissman <jweissman1986@gmail.com>
2020-12-09 17:17:19 -0500

    Updates

---
+++ precache-manifest.5176b5b51d90d8148c9ecfe15047e29e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d114f3a3c19e492bc5d9ca1158de8ca5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29c505f50dd2777695bb",
+    "url": "./static/css/2.9d6f32cf.chunk.css"
+  },
+  {
+    "revision": "f02a8ebfb026277525b2",
+    "url": "./static/css/main.e4c7e0cb.chunk.css"
+  },
+  {
+    "revision": "29c505f50dd2777695bb",
+    "url": "./static/js/2.8cf6ac79.chunk.js"
+  },
+  {
+    "revision": "b6a5f03a88085a1736243eb2a19ab68f",
+    "url": "./static/js/2.8cf6ac79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02a8ebfb026277525b2",
+    "url": "./static/js/main.a94cee3a.chunk.js"
+  },
+  {
+    "revision": "849b6aa675f8588757e0",
+    "url": "./static/js/runtime-main.fe75ea46.js"
+  }
+]);
